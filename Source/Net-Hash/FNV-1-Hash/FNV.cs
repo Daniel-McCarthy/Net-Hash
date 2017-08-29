a858a5a7d26380dc2a594a83ae28ce66936a0355
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-29 15:40:17 -0500

    Added FNV0 Hash 32 Bit Function

--- Source/Net-Hash/FNV-1-Hash/FNV.cs
+++ Source/Net-Hash/FNV-1-Hash/FNV.cs
@@ -7,5 +7,20 @@ namespace FNV_Hash
         private const Int32 fnv032Prime = 0;
         private const Int32 fnv132Prime = unchecked(16777619);//(Int64)(Math.Pow(2, 24) + Math.Pow(2, 8) + 147);
         private const Int32 fnv132Offset = unchecked((int)0x811C9DC5);
+
+
+        public static string fnv032Hash(byte[] message)
+        {
+            Int64 hash = 0;
+
+            foreach(byte data in message)
+            {
+                hash *= fnv032Prime;
+                hash ^= data;
+            }
+
+            return hash.ToString("X8");
+        }
+
     }
 }
