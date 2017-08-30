a2e273cba8808116c41fe8cab90bdb3461988215
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-30 00:21:31 -0500

    Addition of reverseEndian for UInt32 Function to CRC

--- Source/Net-Hash/CRC-Hash/CRC.cs
+++ Source/Net-Hash/CRC-Hash/CRC.cs
@@ -105,6 +105,10 @@ namespace CRC_Hash
             return (~hash).ToString("X8");
         }
 
+        static UInt32 reverseEndian(UInt32 hash)
+        {
+            return (((hash & 0x000000FF) << 24) | ((hash & 0xFF000000) >> 24) | ((hash & 0x00FF0000) >> 8) | ((hash & 0x0000FF00) << 8));
+        }
     }
 
 }
