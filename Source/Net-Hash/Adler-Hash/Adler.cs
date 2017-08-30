c740d4d0ecb037727a37670312c6ab09bd722d73
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-30 00:24:20 -0500

    Addition of adler32 Hash function

--- Source/Net-Hash/Adler-Hash/Adler.cs
+++ Source/Net-Hash/Adler-Hash/Adler.cs
@@ -4,6 +4,23 @@ namespace Adler_Hash
 {
     public class Adler
     {
+        public static string adler32(byte[] message)
+        {
+            UInt32 a = 1;
+            UInt32 b = 0;
+            UInt32 c = 0;
+
+            for(int i = 0; i < message.Length; i++)
+            {
+                c = a;
+                a = (a + message[i]) % 65521;
+                b = (b + message[i] + c) % 65521;
+            }
+
+            UInt64 hash = (UInt64)(b * 0x10000 + a);
+
+            return hash.ToString("X8");
+        }
 
     }
 }
