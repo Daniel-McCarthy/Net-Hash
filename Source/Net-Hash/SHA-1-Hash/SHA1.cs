1e677e888e507b4f628ad01194a37ed64c477c0d
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-06 15:40:02 -0500

    SHA: Added rotation Functions

--- Source/Net-Hash/SHA-1-Hash/SHA1.cs
+++ Source/Net-Hash/SHA-1-Hash/SHA1.cs
@@ -4,5 +4,16 @@ namespace SHA_Hash
 {
     public class SHA
     {
+
+        public static uint rotateLeft(uint value, int amount)
+        {
+            return ((value << amount) | (value >> (32 - amount)));
+        }
+
+        public static uint rotateRight(uint value, int amount)
+        {
+            return ((value >> amount) | (value << (32 - amount)));
+        }
+
     }
 }
