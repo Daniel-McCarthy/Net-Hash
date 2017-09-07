c731cc70d408ce5d0d3be43167699faabc9fd80e
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-07 13:15:50 -0500

    FNV-Hash: Correction to FNV0 Legacy Hash
    -A mistake was made with the implementation of the fnv0 hash. The
    difference between FNV0 and FNV1 is the initialization of the hash
    variable. In FNV0 it is initialized to 0, where as in FNV1 there is a
    specific offset it is set to.
    
    My implementation had set the prime for FNV-0 to 0, which leads to
    drastically different results since it's constantly being multiplied by
    0 each step then having the data spliced in with an XOR operation.

--- Source/Net-Hash/FNV-1-Hash/FNV.cs
+++ Source/Net-Hash/FNV-1-Hash/FNV.cs
@@ -4,7 +4,7 @@ namespace FNV_Hash
 {
     public static class FNV
     {
-        private const Int32 fnv032Prime = 0;
+        private const Int32 fnv032Prime = unchecked(16777619);
         private const Int32 fnv132Prime = unchecked(16777619);//(Int64)(Math.Pow(2, 24) + Math.Pow(2, 8) + 147);
         private const Int32 fnv132Offset = unchecked((int)0x811C9DC5);
 
