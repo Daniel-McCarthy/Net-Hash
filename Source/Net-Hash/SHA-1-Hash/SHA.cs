cb276a06c3134f089bcb4a8ae6028c7c0980e29f
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-08 23:32:03 -0500

    SHA-Hash: Added SHA2 224 bit Hashing

--- Source/Net-Hash/SHA-1-Hash/SHA.cs
+++ Source/Net-Hash/SHA-1-Hash/SHA.cs
@@ -304,6 +304,122 @@ namespace SHA_Hash
 
         }
 
+        public static string sha2_224Hash(byte[] message)
+        {
+
+            {
+                uint a = 0xC1059ED8;
+                uint b = 0x367CD507;
+                uint c = 0x3070DD17;
+                uint d = 0xF70E5939;
+                uint e = 0xFFC00B31;
+                uint f = 0x68581511;
+                uint g = 0x64F98FA7;
+                uint h = 0xBEFA4FA4;
+
+                //Decide padding amount. Padding must always occur, even if length is already divisible by 64.
+                //Space also must be made for an 8 byte message length, and a string terminator character (0x80)
+
+                //If the message length is 9 or more bytes short of 64: Just pad to the nearest multiple of 64
+                //Else: Pad to the nearest multiple 64 and pad another 64 bytes so there is room for the message length
+
+                int paddingAmount = (64 - (message.Length % 64));        //Calculate how many bytes of padding are required to achieve next multiple of 64
+                paddingAmount += (paddingAmount >= 9) ? 0 : 64;          //If less than 9 bytes were padded to the message, add another 64.
+
+
+
+                byte[] paddedMessage = new byte[message.Length + paddingAmount];
+
+                for (int i = 0; i < message.Length; i++)
+                {
+                    paddedMessage[i] = message[i];
+                }
+
+                paddedMessage[message.Length] = 0x80;
+
+                //Write message size to padded message
+                UInt64 sizeToWrite = (UInt64)(message.Length * 8);
+                uint sizeOffset = (uint)(paddedMessage.Length - 8);
+                uint mask = 0x000000FF;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    paddedMessage[(paddedMessage.Length - 1) - i] = (byte)((sizeToWrite & (mask)) >> (i * 8));
+                    mask <<= 8;
+                }
+
+
+                uint[] intConvertedMessage = new uint[64];
+
+                //Loop for each set of 512 bits
+                for (uint j = 0; j < ((paddedMessage.Length << 3) / 512); j++)
+                {
+
+                    //Convert byte array to little endian int array
+                    for (uint i = 0; i < 16; i++)
+                    {
+                        uint fullValue = 0;
+
+                        //No longer reversed endian
+                        fullValue |= (uint)(paddedMessage[(i * 4) + (64 * j)] << 24);
+                        fullValue |= (uint)(paddedMessage[(i * 4) + (64 * j) + 1] << 16);
+                        fullValue |= (uint)(paddedMessage[(i * 4) + (64 * j) + 2] << 8);
+                        fullValue |= (paddedMessage[(i * 4) + (64 * j) + 3]);
+
+                        intConvertedMessage[i] = fullValue;
+
+                    }
+
+                    //Use SHA Calculations to extend to 64 ints
+                    for(int i = 16; i < 64; i++)
+                    {
+                        intConvertedMessage[i] = calculation6(intConvertedMessage[i - 2]) + intConvertedMessage[i - 7] + calculation5(intConvertedMessage[i - 15]) + intConvertedMessage[i - 16];
+                    }
+
+                    uint a1 = a;
+                    uint b1 = b;
+                    uint c1 = c;
+                    uint d1 = d;
+                    uint e1 = e;
+                    uint f1 = f;
+                    uint g1 = g;
+                    uint h1 = h;
+    
+                    for (uint i = 0; i < 64; i++)
+                    {
+                        uint l = 0, m = 0;
+
+                        l = h1 + calculation4(e1) + calculation2(e1, f1, g1) + sha2_224Table[i] + intConvertedMessage[i];
+                        m = calculation3(a1) + calculation1(a1, b1, c1);
+
+                        //Swap variables around and add in previous calculations
+                        h1 = g1;
+                        g1 = f1;
+                        f1 = e1;
+                        e1 = d1 + l;
+                        d1 = c1;
+                        c1 = b1;
+                        b1 = a1;
+                        a1 = l + m; 
+
+                    }
+
+                    a += a1;
+                    b += b1;
+                    c += c1;
+                    d += d1;
+                    e += e1;
+                    f += f1;
+                    g += g1;
+                    h += h1;
+                }
+
+                return a.ToString("X8") + b.ToString("X8") + c.ToString("X8") + d.ToString("X8") + e.ToString("X8") + f.ToString("X8") + g.ToString("X8");
+
+
+            }
+        }
+
         public static uint rotateLeft(uint value, int amount)
         {
             return ((value << amount) | (value >> (32 - amount)));
