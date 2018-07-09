5b08778a4c6dc4c0e0fca1f71b048083f9889def
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2018-07-08 22:17:49 -0500

    Added Hash-Tests XUnit Tests Project

---
+++ Source/Net-Hash/Hash-Tests/Hash-Tests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+using FNV_Hash;
+using CRC_Hash;
+using Adler_Hash;
+using MD_Hash;
+using SHA_Hash;
+
+namespace Hash_Tests
+{
+    public class Hash_Tests
+    {
+    }
+}
